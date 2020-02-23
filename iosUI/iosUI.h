f54917e78d6fecbf21e9d4beb230c1ed1c64fddf
Dennis Kreuzer <dk098@hdm-stuttgart.de>
2020-02-23 13:18:50 +0100

    Initial Commit

---
+++ iosUI/iosUI.h
@@ -0,0 +1,19 @@
+//
+//  iosUI.h
+//  iosUI
+//
+//  Created by Dennis Kreuzer on 23.02.20.
+//  Copyright © 2020 Dennis Kreuzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iosUI.
+FOUNDATION_EXPORT double iosUIVersionNumber;
+
+//! Project version string for iosUI.
+FOUNDATION_EXPORT const unsigned char iosUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosUI/PublicHeader.h>
+
+
